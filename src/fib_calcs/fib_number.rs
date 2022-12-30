aadf085319132ee36501998efe56f77a5c61f924
Seve Martinez <motorific@gmail.com>
2022-12-29 18:44:20 -0700

    cargo edits

---
+++ src/fib_calcs/fib_number.rs
@@ -0,0 +1,14 @@
+use pyo3::prelude::pyfunction;
+
+#[pyfunction]
+pub fn fibonacci_number(n: i32) -> u64 {
+    if n < 0 {
+        panic!("{} cannod be negative", n);
+    }
+
+    match n {
+        0 => panic!("Cannot use zero as an argument"),
+        1 | 2 => 1,
+        _ => fibonacci_number(n - 1) + fibonacci_number(n - 2)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+mod fib_calcs;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+
+
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+
+// pub mod fib_numbers;
 
 #[pyfunction]
 fn say_hello() {
@@ -10,6 +18,8 @@ fn say_hello() {
 #[pymodule]
 fn fib_crust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers))?;
 
     Ok(())
 }
